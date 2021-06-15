7b1f1eaa4892b6a963f75a324e3d5ce7a2c03c60
maccam912 <maccam912@gmail.com>
2021-06-14 22:09:00 -0500

    progress

--- client.go
+++ client.go
@@ -2,12 +2,32 @@ package client
 
 import "github.com/shopspring/decimal"
 
-type Position struct {
+type GTPosition struct {
 	Instrument string
 	Quantity   decimal.Decimal
 	CostBasis  decimal.Decimal
 }
 
+type GTOrderType int
+
+const (
+	MKT GTOrderType = iota
+	LMT
+)
+
+type GTOrder struct {
+	Instrument string
+	Quantity   decimal.Decimal
+	OrderType  GTOrderType
+}
+
 type GTClient interface {
-	CurrentPositions() map[Position]bool
+	OpenPositions() map[GTPosition]bool
+	OpenOrders() map[GTOrder]bool
+	SubmitOrder(order *GTOrder) error
+}
+
+func Buy(client GTClient, instrument string, qty decimal.Decimal) error {
+	o := GTOrder{instrument, qty, MKT}
+	return client.SubmitOrder(&o)
 }
